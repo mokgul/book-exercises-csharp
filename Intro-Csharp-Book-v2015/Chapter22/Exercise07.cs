606e9c23e41887f885a7ce55b93fff0e57982769
Dian Radoslavov <106612784+mokgul@users.noreply.github.com>
2025-08-12 19:40:49 +0300

    CH22-O7-RF

--- Intro-Csharp-Book-v2015/Chapter22/Exercise07.cs
+++ Intro-Csharp-Book-v2015/Chapter22/Exercise07.cs
@@ -1,14 +1,12 @@
+using System.Globalization;
+
 namespace Chapter22;
 
 public static class Exercise07
 {
     public static void UppercaseEveryWord(string s)
     {
-        string[] words = s.Split(' ').Select(w => w.ToLower()).ToArray();
-        words = words.Select(w => w[0].ToString().ToUpper() + w.Substring(1)).ToArray();
-        foreach (var word in words)
-        {
-            Console.Write(word + " ");
-        }
+        string result = new CultureInfo("en-US", false).TextInfo.ToTitleCase(s);
+        Console.WriteLine(result);
     }
 }
